7818297416b119f5d1a0da70e088354ef94a1224
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2023-04-21 11:50:48 +0530

    create method handler

--- index.js
+++ index.js
@@ -3,4 +3,47 @@ const express = require('express');
 const cors = require('cors');
 const commandLineArgs = require('command-line-args');
 const config = require('config');
-console.log('Hello World '+config.get('server.port'));
+const app = express()
+
+app.use(morgan('dev'));
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+  app.use((req, res, next) => {
+    console.log('Time:', Date.now())
+    next()
+  })
+
+  app.get('/', (req, res) => {
+    res.send('get')
+  })
+ 
+  app.put('/', (req, res) => {
+    res.send('put')
+  })
+
+  app.post('/', (req, res) => {
+    res.send('post')
+  })
+
+  app.delete('/', (req, res) => {
+    res.send('delete')
+  })
+
+  app.options('/', (req, res) => {
+    res.send('options')
+  })
+
+  app.patch('/', (req, res) => {
+    res.send('patch')
+  })
+
+  app.delete('/', (req, res) => {
+    res.send('delete')
+  })
+
+  app.listen(config.get('server.port'), function () {
+    console.log('app listening on port '+config.get('server.port'));
+  });
