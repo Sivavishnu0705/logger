d2bb4b57a09700a3afd8c1ed3a1841b8b52164e9
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2023-04-22 12:04:11 +0530

    coloured logs

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 
-function localLog(req, res,method){
+function localLog(req, res){
     const winston = require('winston');
     require('winston-daily-rotate-file');
     
     const { combine, timestamp, json } = winston.format;
     
     const fileRotateTransport = new winston.transports.DailyRotateFile({
-      filename: './logs/%DATE%_'+Date.now()+'_'+method+'.js',
+      filename: './logs/%DATE%_'+Date.now()+'_'+req.method+'.js',
       datePattern: 'YYYY-MM-DD',
       maxFiles: config.get('server.rotating_logs_expiry'),
     });
@@ -29,6 +29,17 @@ function localLog(req, res,method){
     logger.info('Request : '+JSON.stringify(req.body)+', Response : '+res);
 }
 
+function colouredLog(req,res){
+    const statusCode = res.statusCode;
+if(statusCode >= 200 && statusCode<300){
+    console.log('\x1b[32m%s\x1b[0m', 'Status Code :'+statusCode+', Method : '+req.method)
+}else if(statusCode>=400){
+    console.log('\x1b[31m%s\x1b[0m', 'Status Code :'+statusCode+', Method : '+req.method)
+}else {
+    console.log('\x1b[33m%s\x1b[0m', 'Status Code :'+statusCode+', Method : '+req.method)
+}
+}
+
 app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
@@ -36,42 +47,48 @@ app.use((err, req, res, next) => {
 
   app.use((req, res, next) => {
     console.log('Time:', Date.now())
-
     next()
   })
 
   app.get('/', (req, res) => {
     res.send('get')
+    colouredLog(req,res);
     localLog(req,'get','get');
   })
  
   app.put('/', (req, res) => {
     res.send('put')
+    colouredLog(req,res);
     localLog(req,'put','put');
 })
 
   app.post('/', (req, res) => {
     res.send('post '+req.body.test)
+    colouredLog(req,res);
     localLog(req,'post '+req.body.test,'post');
 })
 
   app.delete('/', (req, res) => {
     res.send('delete')
+    colouredLog(req,res);
     localLog(req,'delete','delete');
 })
 
   app.options('/', (req, res) => {
     res.send('options')
+    colouredLog(req,res);
     localLog(req,'options','options');
 })
 
   app.patch('/', (req, res) => {
     res.send('patch')
+    colouredLog(req,res);
     localLog(req,'patch','patch');
 })
 
   app.delete('/', (req, res) => {
     res.send('delete')
+    colouredLog(req,res);
     localLog(req,'delete','delete');
 })
 
