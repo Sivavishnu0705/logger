e4cab67cac9248280732838deda18d118829f0ad
Sivavishnu0705 <32070669+Sivavishnu0705@users.noreply.github.com>
2023-04-28 08:53:32 +0530

    update ESlint

--- index.js
+++ index.js
@@ -1,49 +1,77 @@
 const morgan = require('morgan')
-const express = require('express'), bodyParser = require('body-parser')
-const cors = require('cors')
-const commandLineArgs = require('command-line-args')
+const express = require('express')
+const bodyParser = require('body-parser')
 const config = require('config')
-const app = express()
-const logger = require('./logger.js')
-const CircularJSON = require('circular-json')
 
+const app = express()
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-  })
-
-  app.use((req, res, next) => {
-     next()
-  })
-
-  app.get('/', (req, res) => {
-    res.send('get')
-  })
- 
-  app.put('/', (req, res) => {
-    res.send('put')
-  })
-
-  app.post('/', (req, res) => {
-    res.send('post '+req.body.test)
-  })
-
-  app.delete('/', (req, res) => {
-    res.send('delete')
+app.use((err, req, res) => {
+  res.status(500).send('Something broke!')
 })
 
-  app.patch('/', (req, res) => {
-    res.send('patch')
+app.use((req, res, next) => {
+  next()
 })
 
-  app.delete('/', (req, res) => {
-    res.send('delete')
+app.get('/', (req, res) => {
+  res.send('get')
 })
 
-  app.listen(config.get('server.port'), function () {
-    console.log('app listening on port '+config.get('server.port'))
-  })
+app.put('/', (req, res) => {
+  res.send('put')
+})
+
+app.post('/', (req, res) => {
+  res.send(`post ${req.body.test}`)
+})
+
+app.delete('/', (req, res) => {
+  res.send('delete')
+})
+
+app.delete('/', (req, res) => {
+  res.send('delete')
+})
+
+app.delete('/', (req, res) => {
+  res.send('delete')
+})
+
+app.delete('/', (req, res) => {
+  res.send('delete')
+})
+
+app.delete('/', (req, res) => {
+  res.send('delete')
+})
+
+app.patch('/', (req, res) => {
+  res.send('patch')
+})
+
+app.delete('/', (req, res) => {
+  res.send('delete')
+})
+
+app.options('/', (req, res) => {
+  res.send('options')
+})
+
+app.trace('/', (req, res) => {
+  res.send('trace')
+})
+
+app.connect('/', (req, res) => {
+  res.send('connect')
+})
+
+app.head('/', (req, res) => {
+  res.send('head')
+})
+
+app.listen(config.get('server.port'), () => {
+  console.log(`app listening on port ${config.get('server.port')}`)
+})
